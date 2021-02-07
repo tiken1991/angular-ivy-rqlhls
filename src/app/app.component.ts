31fb836ed495d5284cd1575f7f3903a474a687cc
tiken1991 <timm@kenfenheuer.de>
2021-02-07 23:39:23 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component, VERSION } from '@angular/core';
+import {
+  animate,
+  animation,
+  keyframes,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger
+} from "@angular/animations";
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  animations: [
+    trigger("rect", [
+      transition(":enter", [
+        query("rect, :enter", [
+          stagger(300, [
+            style({ width: 0 }),
+            animate(
+              "1s",
+              keyframes([
+                style({ width: "0%" }),
+                style({ width: "200%" }),
+                style({ width: "*" })
+              ])
+            )
+          ])
+        ])
+      ])
+    ])
+  ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
 }
